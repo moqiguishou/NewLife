39aee710051dc3ac942fc6a82b3dd5b374de9231
nizw <612133@nd.com>
2019-02-14 19:05:34 +0800

    11

---
+++ NewLife/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace NewLife {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btn_test = new System.Windows.Forms.Button();
+            this.txt_test = new System.Windows.Forms.TextBox();
+            this.txt_2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(314, 114);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_test.TabIndex = 0;
+            this.btn_test.Text = "button1";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // txt_test
+            // 
+            this.txt_test.Location = new System.Drawing.Point(188, 190);
+            this.txt_test.Multiline = true;
+            this.txt_test.Name = "txt_test";
+            this.txt_test.Size = new System.Drawing.Size(343, 96);
+            this.txt_test.TabIndex = 1;
+            this.txt_test.TextChanged += new System.EventHandler(this.txt_test_TextChanged);
+            // 
+            // txt_2
+            // 
+            this.txt_2.Location = new System.Drawing.Point(219, 299);
+            this.txt_2.Name = "txt_2";
+            this.txt_2.Size = new System.Drawing.Size(100, 21);
+            this.txt_2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_2);
+            this.Controls.Add(this.txt_test);
+            this.Controls.Add(this.btn_test);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.TextBox txt_test;
+        private System.Windows.Forms.TextBox txt_2;
+    }
+}
+
---
+++ NewLife/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace NewLife {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btn_test_Click(object sender, EventArgs e) {
+            string worksheetname = string.Empty;
+
+            //导入本地文件  
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "文档(*.xls)|*.xlsx";
+            if (file.ShowDialog() == DialogResult.OK) {
+                txt_test.Text = file.FileName;
+            }
+            //判断有没有文件导入  
+            if (file.FileName.Length == 0) {
+                MessageBox.Show("请选择要导入的文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //======================================//
+            Excel.Application excel = new Excel.Application();
+            Excel.Workbook wb = null;
+            wb = excel.Workbooks.Open(file.FileName);
+            Excel.Worksheet ws = wb.Worksheets[1];
+            int nRow = 0;
+            int nColum = 0;
+            string otxt_test = "";
+            try {
+                nRow = ws.UsedRange.Rows.Count;
+                nColum = ws.UsedRange.Columns.Count;
+                MessageBox.Show(nRow.ToString(),"", MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+                string ordernum = string.Empty;
+                string count = string.Empty;
+                //循环行
+                for (int i = 1; i <= nRow; i++)//
+                {
+                    for (int j = 1; j <= nColum; j++) {
+                        if (ws.Cells[i,j] != null) {
+                            otxt_test = otxt_test + ws.Cells[i, j].Text;
+                        }
+                    }
+                }
+                txt_test.Text = otxt_test;
+                
+            } catch (Exception) {
+
+                throw;
+            }
+           
+        }
+
+        private void txt_test_TextChanged(object sender, EventArgs e) {
+            txt_2.Text = "hello";
+        }
+    }
+}
